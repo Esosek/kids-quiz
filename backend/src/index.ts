3167a3d96886ce235a40ba19189af16df8e219c7
Aleš Zima <zima.ales@seznam.cz>
2025-07-24 12:42:08 +0200

    Added basic express app with dependencies

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+const PORT = 8080
+
+app.get('/', (_req, res) => res.send('Hello World!'))
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
