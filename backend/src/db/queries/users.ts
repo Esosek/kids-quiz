b6511d730b7629aaddfc5ffbcd3b072c52df7ca8
Aleš Zima <zima.ales@seznam.cz>
2025-07-28 21:04:58 +0200

    Added getUserByName query

--- backend/src/db/queries/users.ts
+++ backend/src/db/queries/users.ts
@@ -1,14 +1,37 @@
+import { eq } from 'drizzle-orm'
+
 import { db } from '../../db/index'
 import { type User, users } from '../../db/schema'
+import { NotFoundError } from 'src/types/errors'
+import { hashPassword } from 'src/auth'
 
 type UserResponse = Omit<User, 'hashedPassword'>
 
 export async function createUser(username: string, password: string): Promise<UserResponse> {
   try {
-    const [result] = await db.insert(users).values({ username, hashedPassword: password }).returning()
+    const [result] = await db
+      .insert(users)
+      .values({ username, hashedPassword: await hashPassword(password) })
+      .returning()
     const { hashedPassword, ...userResponse } = result
     return userResponse
   } catch (error) {
+    console.log(error)
     throw new Error('Creating user failed')
+  }
+}
+
+export async function getUserByName(username: string): Promise<UserResponse> {
+  try {
+    const [result] = await db.select().from(users).where(eq(users.username, username))
+    if (!result) {
+      throw new NotFoundError('User not found')
+    }
+    const { hashedPassword, ...userResponse } = result
+    return userResponse
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      throw error
+    } else throw new Error('Retrieving user failed')
   }
 }
