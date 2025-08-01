8a349c5928f9ebc9ce77b9d0091a267b4140d262
Aleš Zima <zima.ales@seznam.cz>
2025-08-01 11:30:52 +0200

    Added userUnlocks table to schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -11,49 +11,45 @@ export type User = typeof users.$inferSelect
 export type Category = typeof categories.$inferSelect
 export type Subcategory = typeof subcategories.$inferSelect
 export type Question = typeof questions.$inferSelect
+export type UserUnlock = typeof userUnlocks.$inferSelect
 
 export const users = pgTable('users', {
   id: uuid('id').primaryKey().defaultRandom(),
+  username: varchar('username', { length: 64 }).unique().notNull(),
+  hashedPassword: varchar('hashed_password').default('unset').notNull(),
+  currency: integer('currency').default(0).notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at')
     .notNull()
     .defaultNow()
     .$onUpdate(() => new Date()),
-  username: varchar('username', { length: 64 }).unique().notNull(),
-  hashedPassword: varchar('hashed_password').default('unset').notNull(),
-  currency: integer('currency').default(0).notNull(),
 })
 
 export const categories = pgTable('categories', {
   id: uuid('id').primaryKey().defaultRandom(),
+  label: varchar('label', { length: 64 }).unique().notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at')
     .notNull()
     .defaultNow()
     .$onUpdate(() => new Date()),
-  label: varchar('label', { length: 64 }).unique().notNull(),
 })
 
 export const subcategories = pgTable('subcategories', {
   id: uuid('id').primaryKey().defaultRandom(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at')
-    .notNull()
-    .defaultNow()
-    .$onUpdate(() => new Date()),
   label: varchar('label', { length: 64 }).unique().notNull(),
   categoryId: uuid('category_id').references(() => categories.id, {
     onDelete: 'set null',
   }),
-})
-
-export const questions = pgTable('questions', {
-  id: uuid('id').primaryKey().defaultRandom(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at')
     .notNull()
     .defaultNow()
     .$onUpdate(() => new Date()),
+})
+
+export const questions = pgTable('questions', {
+  id: uuid('id').primaryKey().defaultRandom(),
   text: text('text'),
   answer: text('answer').notNull(),
   imgUrl: varchar('img_url'),
@@ -62,4 +58,24 @@ export const questions = pgTable('questions', {
     .references(() => subcategories.id, {
       onDelete: 'cascade',
     }),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+})
+
+export const userUnlocks = pgTable('user_unlocks', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  userId: uuid('user_id')
+    .references(() => users.id)
+    .notNull(),
+  subcategoryId: uuid('subcategory_id')
+    .references(() => subcategories.id)
+    .notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
 })
