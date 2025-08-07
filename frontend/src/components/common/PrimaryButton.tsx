6cfef9f1909df21386eb13f74628ab5a02f1719b
Aleš Zima <zima.ales@seznam.cz>
2025-08-07 20:33:51 +0200

    Added PrimaryButton component

---
+++ frontend/src/components/common/PrimaryButton.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+
+type PrimaryButtonProps = {
+  children: ReactNode
+  onClick?: () => void
+  className?: string
+}
+
+export default function PrimaryButton(props: PrimaryButtonProps) {
+  return (
+    <button
+      onClick={props.onClick}
+      className={`${
+        props.className ?? 'bg-green-500 hover:bg-green-400'
+      } text-2xl font-medium py-3 px-9 w-full max-w-lg rounded-full shadow-lg`}
+    >
+      {props.children}
+    </button>
+  )
+}
