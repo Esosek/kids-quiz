8dcf00e5c9d8cf223a8235a702c0a39a7bdb0159
Aleš Zima <zima.ales@seznam.cz>
2025-08-07 20:39:26 +0200

    Added LinkButton component

---
+++ frontend/src/components/common/LinkButton.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react'
+
+type LinkButtonProps = {
+  children: ReactNode
+  onClick?: () => void
+  className?: string
+}
+
+export default function LinkButton(props: LinkButtonProps) {
+  return (
+    <button
+      onClick={props.onClick}
+      className={`${props.className} underline mt-4`}
+    >
+      {props.children}
+    </button>
+  )
+}
