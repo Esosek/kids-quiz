bf3eb4ca5812ff699e3d6aa5251de87428666a41
Aleš Zima <zima.ales@seznam.cz>
2025-08-08 11:33:12 +0200

    Addded TextInput component

--- frontend/src/components/AuthPage.tsx
+++ frontend/src/components/AuthPage.tsx
@@ -1,19 +1,57 @@
+import Image from 'next/image'
+
+import logo from '@/assets/logo.png'
 import { useUserStore } from '@/stores/user_store'
+import PrimaryButton from './common/PrimaryButton'
+import LinkButton from './common/LinkButton'
+import { FormEvent, useState } from 'react'
+import TextInput from './common/TextInput'
+import AvatarPicker from './AvatarPicker'
 
 export default function AuthPage() {
   const login = useUserStore((state) => state.login)
+  const [keepLoggedIn, setKeepLoggedIn] = useState(true)
+  const [isRegistrationOpen, setIsRegistrationOpen] = useState(false)
 
   const handleLogin = () => login('Alfik', 'password')
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    console.log(event.target)
+  }
 
   return (
-    <>
-      <h1 className='text-4xl'>AUTH PAGE</h1>
-      <button
-        className='bg-red-500 py-1 px-4 rounded-2xl hover:bg-red-300'
-        onClick={handleLogin}
-      >
-        login
-      </button>
-    </>
+    <form onSubmit={handleSubmit} className='grid justify-items-center'>
+      <div className='max-w-[80%] h-auto relative flex flex-col items-center'>
+        <Image
+          src={logo}
+          alt='Logo aplikace'
+          width={512}
+          height={512}
+          priority
+        />
+      </div>
+      <TextInput id='username' placeholder='PŘEZDÍVKA' />
+      <TextInput id='password' type='password' placeholder='HESLO' />
+      <div className='justify-self-start m-2 ml-4 flex items-center'>
+        <input
+          type='checkbox'
+          name='keep-logged'
+          id='logged'
+          className=' accent-green-500 size-6'
+          checked={keepLoggedIn}
+          onChange={() => setKeepLoggedIn(!keepLoggedIn)}
+        />
+        <label htmlFor='keep-logged' className='uppercase ml-2'>
+          zůstat přihlášen
+        </label>
+      </div>
+      <PrimaryButton onClick={handleLogin}>
+        {isRegistrationOpen ? 'VYTVOŘIT ÚČET' : 'PŘIHLÁSIT'}
+      </PrimaryButton>
+      {isRegistrationOpen && <AvatarPicker />}
+      <LinkButton onClick={() => setIsRegistrationOpen(!isRegistrationOpen)}>
+        {isRegistrationOpen ? 'ZPÁTKY K PŘIHLÁŠENÍ' : 'JSI TU POPRVÉ?'}
+      </LinkButton>
+    </form>
   )
 }
---
+++ frontend/src/components/common/TextInput.tsx
@@ -0,0 +1,31 @@
+type TextInputProps = {
+  id: string
+  type?: string
+  placeholder?: string
+  error?: string
+}
+
+export default function TextInput(props: TextInputProps) {
+  return (
+    <div
+      className={`w-full border-2 rounded-full my-1 relative focus-within:border-green-500 focus-within:ring-green-500 focus-within:ring-1 ${
+        props.error ? 'mt-4 border-red-600' : 'border-green-700'
+      }`}
+    >
+      {props.error && (
+        <p className='absolute text-red-600 uppercase text-sm -top-5 left-5'>
+          {props.error}
+        </p>
+      )}
+      <input
+        name={props.id}
+        id={props.id}
+        type={props.type ?? 'text'}
+        placeholder={props.placeholder}
+        className={`text-2xl uppercase text-center w-full h-full py-3 rounded-full overflow-clip focus:outline-none placeholder:text-green-800/45 ${
+          props.error ? 'text-red-600' : 'text-black'
+        }`}
+      />
+    </div>
+  )
+}
