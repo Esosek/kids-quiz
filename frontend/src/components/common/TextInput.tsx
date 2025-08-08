a5f045761a92c7657a5586308342e7d7dcd418fd
Aleš Zima <zima.ales@seznam.cz>
2025-08-08 17:06:41 +0200

    Added password visibility button to TextInput

--- frontend/src/components/common/TextInput.tsx
+++ frontend/src/components/common/TextInput.tsx
@@ -1,3 +1,9 @@
+import Image from 'next/image'
+import { useState } from 'react'
+
+import iconVisibility from '@/assets/icon_visibility.svg'
+import iconVisibilityOff from '@/assets/icon_visibility_off.svg'
+
 type TextInputProps = {
   id: string
   value?: string
@@ -8,9 +14,24 @@ type TextInputProps = {
 }
 
 export default function TextInput(props: TextInputProps) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+
+  let type = 'text'
+
+  if (props.type) {
+    type = props.type
+  }
+
+  if (isPasswordVisible) {
+    type = 'text'
+  }
+
+  function handleToggleVisibility() {
+    setIsPasswordVisible(!isPasswordVisible)
+  }
   return (
     <div
-      className={`w-full border-2 rounded-full my-1 relative focus-within:border-green-500 focus-within:ring-green-500 focus-within:ring-1 ${
+      className={`relative w-full border-2 rounded-full my-1 focus-within:border-green-500 focus-within:ring-green-500 focus-within:ring-1 ${
         props.error ? 'mt-4 border-red-600' : 'border-green-700'
       }`}
     >
@@ -22,7 +43,7 @@ export default function TextInput(props: TextInputProps) {
       <input
         name={props.id}
         id={props.id}
-        type={props.type ?? 'text'}
+        type={type}
         placeholder={props.placeholder}
         value={props.value}
         onChange={
@@ -32,6 +53,21 @@ export default function TextInput(props: TextInputProps) {
           props.error ? 'text-red-600' : 'text-black'
         }`}
       />
+      <button
+        type='button'
+        onClick={handleToggleVisibility}
+        className='absolute right-8 top-0 bottom-0'
+      >
+        {props.type === 'password' && (
+          <Image
+            src={isPasswordVisible ? iconVisibilityOff : iconVisibility}
+            width={24}
+            height={24}
+            alt='Eye icon'
+            className='size-10 opacity-50'
+          />
+        )}
+      </button>
     </div>
   )
 }
