b93162ae394c840a8c0bb5fca34cc00685041f66
Aleš Zima <zima.ales@seznam.cz>
2025-08-08 14:01:49 +0200

    Added type props to Button components

--- frontend/src/components/common/LinkButton.tsx
+++ frontend/src/components/common/LinkButton.tsx
@@ -4,12 +4,14 @@ type LinkButtonProps = {
   children: ReactNode
   onClick?: () => void
   className?: string
+  type?: 'button' | 'submit' | 'reset' | undefined
 }
 
 export default function LinkButton(props: LinkButtonProps) {
   return (
     <button
       onClick={props.onClick}
+      type={props.type ?? 'button'}
       className={`${props.className ?? ''} underline my-4`}
     >
       {props.children}
--- frontend/src/components/common/PrimaryButton.tsx
+++ frontend/src/components/common/PrimaryButton.tsx
@@ -4,12 +4,14 @@ type PrimaryButtonProps = {
   children: ReactNode
   onClick?: () => void
   className?: string
+  type?: 'button' | 'submit' | 'reset' | undefined
 }
 
 export default function PrimaryButton(props: PrimaryButtonProps) {
   return (
     <button
       onClick={props.onClick}
+      type={props.type ?? 'button'}
       className={`${
         props.className ?? 'bg-green-500'
       } text-2xl font-medium py-3 px-9 w-full max-w-lg rounded-full shadow-lg my-4`}
