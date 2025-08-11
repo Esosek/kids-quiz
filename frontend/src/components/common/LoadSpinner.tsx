a77a3aca19cbf93ca29f210a642f9eb109b128b7
Aleš Zima <zima.ales@seznam.cz>
2025-08-11 08:39:54 +0200

    Added LoadSpinner component

---
+++ frontend/src/components/common/LoadSpinner.tsx
@@ -0,0 +1,5 @@
+export default function LoadSpinner() {
+  return (
+    <span className='inline-block animate-spin border-4 border-b-transparent rounded-[50%] border-blue-600 size-7 mx-auto'></span>
+  )
+}
