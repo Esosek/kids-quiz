41fb24a35b0ffdd72a4f2ddb56aaf21363fa2cea
Aleš Zima <zima.ales@seznam.cz>
2025-08-13 19:14:15 +0200

    Improved QuizProgressTracker

--- frontend/src/components/quiz/QuizProgressTracker.tsx
+++ frontend/src/components/quiz/QuizProgressTracker.tsx
@@ -1,14 +1,15 @@
 import AnswerIndicator from './AnswerIndicator'
 
 type QuizProgressTrackerProps = {
-  answers: Array<boolean | 'current' | undefined>
+  answers: Array<boolean | undefined>
 }
 
 export default function QuizProgressTracker({ answers }: QuizProgressTrackerProps) {
+  const currentIndex = answers.findIndex((a) => a === undefined)
   return (
     <div className='flex gap-4'>
       {answers.map((a, index) => (
-        <AnswerIndicator key={index} answer={a} />
+        <AnswerIndicator key={index} answer={index === currentIndex ? 'current' : a} />
       ))}
     </div>
   )
