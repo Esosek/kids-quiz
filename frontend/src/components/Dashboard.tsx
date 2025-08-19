4e23c11d7feeaa3e542575131043ee32468f2e0e
Aleš Zima <zima.ales@seznam.cz>
2025-08-19 15:37:04 +0200

    Moved admin section link to header

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,14 +1,11 @@
-import Link from 'next/link'
 import { useState } from 'react'
 
 import Header from './Header'
 import SubcategoryCard from './subcategory/SubcategoryCard'
 import { useCategoryStore } from '@/stores/category_store'
 import Dropdown from './common/Dropdown'
-import { useUserStore } from '@/stores/user_store'
 
 export default function Dashboard() {
-  const user = useUserStore((state) => state.user)
   const { subcategories, categories } = useCategoryStore()
   const sortedSubcategories = [...Object.values(subcategories!)].sort((a, b) => {
     if (a.isUnlocked && !b.isUnlocked) return -1
@@ -45,14 +42,7 @@ export default function Dashboard() {
   return (
     <main>
       <Header />
-      <div className='flex justify-between items-center'>
-        <Dropdown options={filterOptions} onChange={handleFilter} />
-        {user?.id === process.env.NEXT_PUBLIC_ADMIN_ID && (
-          <Link href={'/admin'} className='uppercase underline'>
-            admin sekce
-          </Link>
-        )}
-      </div>
+      <Dropdown options={filterOptions} onChange={handleFilter} />
       <ul className='gap-8 grid auto-rows-fr grid-cols-1 sm:grid-cols-2 w-full max-w-sm sm:max-w-none'>
         {Object.values(filteredSubcategories).map((sub) => (
           <SubcategoryCard key={sub.id} subcategory={sub} />
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 
 import { useUserStore } from '@/stores/user_store'
 import iconLogout from '@/assets/icon_logout.svg'
@@ -7,18 +9,24 @@ import { useCurrencyStore } from '@/stores/currency_store'
 import CurrencyDisplay from './CurrencyDisplay'
 
 export default function Header() {
+  const pathName = usePathname()
   const { user, logout } = useUserStore()
   const { currency, addCurrency } = useCurrencyStore()
   return (
     <header className='relative flex justify-between items-start w-full mb-8 sm:mb-10'>
       <CurrencyDisplay value={currency} />
       {user?.id === process.env.NEXT_PUBLIC_ADMIN_ID && (
-        <button
-          className='absolute left-0 bottom-2 bg-green-500 py-1 px-2 text-sm rounded-2xl'
-          onClick={() => addCurrency(25)}
-        >
-          +25
-        </button>
+        <>
+          <Link href={pathName === '/admin' ? '/' : '/admin'} className='uppercase underline'>
+            {pathName === '/admin' ? 'dashboard' : 'admin sekce'}
+          </Link>
+          <button
+            className='absolute left-0 bottom-2 bg-green-500 py-1 px-2 text-sm rounded-2xl'
+            onClick={() => addCurrency(25)}
+          >
+            +25
+          </button>
+        </>
       )}
       <div className='grid justify-items-center gap-1'>
         <div className='size-10'>
