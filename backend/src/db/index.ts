754728df2ba1aea4681d9fa6770f24eff81e4a13
Aleš Zima <zima.ales@seznam.cz>
2025-08-22 13:23:46 +0200

    Updated postgres config

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -4,5 +4,5 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import * as schema from '../db/schema'
 import config from '../config'
 
-const conn = postgres(config.db.url)
+const conn = postgres(config.db.url, { prepare: false })
 export const db = drizzle(conn, { schema })
