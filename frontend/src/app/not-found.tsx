dab9c033d6b7238d6f1b4832c0f2df454b535d2f
Aleš Zima <zima.ales@seznam.cz>
2025-08-23 20:27:44 +0200

    Added custom 404 page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div className='flex flex-col items-center justify-center h-full gap-4'>
+      <div className='text-8xl'>404</div>
+      <div className='uppercase'>stránka neexistuje...</div>
+    </div>
+  )
+}
